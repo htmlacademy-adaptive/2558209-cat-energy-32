4039bc1deb40cea25eb3295f5ccd855599cf7f92
KarinaDemchenko02 <146772294+KarinaDemchenko02@users.noreply.github.com>
2024-09-21 14:45:24 +0200

    Сделана сетка (без pixel perfect) (#8)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
-/* в этот файл добавляет скрипты*/
+// eslint-disable-next-line check-file/folder-naming-convention
+const buttonMenu = document.querySelector('.button-burger');
+const menu = document.querySelector('.nav');
+
+buttonMenu.addEventListener('click', () => {
+  buttonMenu.classList.toggle('button-burger--close');
+  menu.classList.toggle('nav--open');
+});
