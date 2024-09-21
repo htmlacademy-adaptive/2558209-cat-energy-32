c8eb0e31cd6a62bcaeb00c78fa3dc81bf688004f
KarinaDemchenko02 <146772294+KarinaDemchenko02@users.noreply.github.com>
2024-09-22 01:00:29 +0200

    Module6 task1 (#11)
    * Сделана сетка (без pixel perfect)
    
    * изменен импорт блока page
    
    * Добавлены эффекты
    
    * Исправление ошибок

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,9 @@
 const buttonMenu = document.querySelector('.button-burger');
 const menu = document.querySelector('.nav');
 
+buttonMenu.classList.remove('button-burger--close');
+menu.classList.remove('nav--open-nojs');
+
 buttonMenu.addEventListener('click', () => {
   buttonMenu.classList.toggle('button-burger--close');
   menu.classList.toggle('nav--open');
